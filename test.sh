5707491944e95ca0d4d73bf3bb7e08d438745aa9
Lucas Wilson <lkwilson96@gmail.com>
2021-12-25 09:47:58 -1000

    Add test script

---
+++ test.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+
+bad() {
+  echo "Test failed!"
+  exit 1
+}
+
+set -ex
+rm -rf .rit
+verbose=-v
+python rit.py $verbose init || bad
+python rit.py $verbose init && bad
+python rit.py $verbose branch first && bad
+python rit.py $verbose commit "Hey there" || bad
+python rit.py $verbose branch first || bad
+python rit.py $verbose branch || bad
+python rit.py $verbose commit "Oh hey there" || bad
+python rit.py $verbose branch || bad
+python rit.py $verbose branch new || bad
+python rit.py $verbose branch || bad
+python rit.py $verbose commit "move main" || bad
+python rit.py $verbose branch || bad
+python rit.py $verbose branch new main && bad
+python rit.py $verbose branch || bad
+python rit.py $verbose branch new main -f || bad
+python rit.py $verbose branch || bad
+python rit.py $verbose branch -d new main && bad
+python rit.py $verbose branch -d -f && bad
+python rit.py $verbose branch -d new -f && bad
+python rit.py $verbose branch -d && bad
+python rit.py $verbose branch || bad
+python rit.py $verbose branch -d new || bad
